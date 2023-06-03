00d313094d3067a117c434ebace71853ebf3c837
Uttam <utam.shrestha65@gmail.com>
2023-06-03 14:25:43 +0545

    URL is correctly pointing to the link

--- application/views.py
+++ application/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def projects(request):
+    return HttpResponse("These are the projects.")
+
+def project(request, pk):
+    return HttpResponse("Single project " + " " + str(pk))
