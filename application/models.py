524a9f745b641d558f16118f3a04c178f3c993bd
Uttam <utam.shrestha65@gmail.com>
2023-06-04 15:56:54 +0545

    Created super user and models and added some raw data

--- application/models.py
+++ application/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Project (models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField(blank = True, null = True)
+    demo_link = models.CharField(max_length=2000, blank = True, null = True)
+    source_link = models.CharField(max_length = 2000, blank = True, null = True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return self.title
