48275844f712e1c7a0a7858eb0a733e8d5dfff43
Uttam <utam.shrestha65@gmail.com>
2023-06-05 17:47:22 +0545

    Querying database objects in templates

--- application/views.py
+++ application/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
+
 
 # Create your views here.
 def projects(request):
-    return render(request, 'application/projects.html')
+    projects = models.Project.objects.all() # quering all the projects and storing in projects
+    context = {'projects': projects}
+    return render(request, 'application/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'application/single__project.html')
+    projectObj = models.Project.objects.get(id=pk) # single project should match with it's id.
+    context = {
+        'project': projectObj
+    }
+    return render(request, 'application/single__project.html', context)
