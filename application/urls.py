addc8c132411db51bbcb77a19dc6026453791335
Uttam Shrestha <66063634+uttamshr10@users.noreply.github.com>
2023-06-12 05:20:59 +0545

    Added few comments.

--- application/urls.py
+++ application/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("projects/", views.projects, name="projects"),
-    path("project/<str:pk>/", views.project, name="project"),
-    path("create-project/", views.createProject, name = "create"),
-    path("update-project/<str:pk>/", views.updateProject, name = 'update'),
-    path("delete-project/<str:pk>/", views.deleteProject, name = 'delete'),
+    # urls for function based views
+    path("projects/", views.projects, name="projects"),    # list view
+    path("project/<str:pk>/", views.project, name="project"),   # detail view 
+    path("create-project/", views.createProject, name = "create"),   # create view
+    path("update-project/<str:pk>/", views.updateProject, name = 'update'),  # update view
+    path("delete-project/<str:pk>/", views.deleteProject, name = 'delete'),  # delete view
 
     # CRUD OPERATION WITH GENERIC VIEWS.
     # path("create-project", views.CreateProject.as_view(), name = 'create'),
