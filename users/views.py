77995d9fdf28794152ba5ffe4a8137958bb03e94
Uttam <utam.shrestha65@gmail.com>
2023-06-16 15:08:27 +0545

    Profiles template added

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from users import models
 
-# Create your views here.
-
-def Profile(request):
-    return render(request, 'users/profiles.html')
+def Profile(request):   
+    profiles = models.Profile.objects.all() # query all the profiles and store in profiles.
+    context = {'profiles': profiles}    # set profiles as profiles to use in template later.
+    return render(request, 'users/profiles.html', context)  # pass context to use those variables in template.
